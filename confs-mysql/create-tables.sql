70a833a7f9981645cb72eae55114e8d0326a8179
Gabriel Ferreira <84422577+GabsFerrarii@users.noreply.github.com>
2023-12-17 19:51:35 -0300

    Update create-tables.sql

--- confs-mysql/create-tables.sql
+++ confs-mysql/create-tables.sql
@@ -1,29 +1,27 @@
 CREATE TABLE scoobyburg.cliente (
 	senha varchar(20) NOT NULL,
 	nome varchar(50) NOT NULL,
-	email varchar(50) NOT NULL,
-	CONSTRAINT cliente_pk PRIMARY KEY (email)
+	email varchar(50) NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE scoobyburg.hamburger (
+CREATE TABLE scoobyburg.hamburguer (
 	nome varchar(30) NOT NULL,
-	decricao varchar(100) NOT NULL,
-	id int NOT NULL,
-	column1 bytea NOT NULL,
+	descricao varchar(100) NOT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,
+	foto binary NOT NULL,
 	preco float4 NOT NULL
-	CONSTRAINT hamburger_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE scoobyburg.bebida (
 	nome varchar(30) NOT NULL,
 	descricao varchar(100) NOT NULL,
-	id int NOT NULL,
-	foto bytea NOT NULL,
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	foto binary NOT NULL,
 	preco float4 NOT NULL
 );
 
 CREATE TABLE scoobyburg.compra (
-	id int NOT NULL,
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	local varchar(50) NOT NULL,
 	forma_pagamento char(1) NOT NULL,
 	dt_hora timestamp NOT NULL,
