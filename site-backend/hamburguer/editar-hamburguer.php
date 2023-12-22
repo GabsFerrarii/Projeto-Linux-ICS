b388f2fc33ad80f97c530bbc5ce6cc61651ef45d
Gabriel Ferreira <84422577+GabsFerrarii@users.noreply.github.com>
2023-12-21 22:22:52 -0300

    Update editar-hamburguer.php

--- site-backend/hamburguer/editar-hamburguer.php
+++ site-backend/hamburguer/editar-hamburguer.php
@@ -20,20 +20,20 @@
     <div class="w-[70rem] flex justify-between items-center">
       <h1>
         <a href="index.html">
-          <img src="../imagens/logo.svg">
+          <img src="../../imagens/logo.svg">
         </a>
       </h1>
       <ol class="flex justify-between text-2xl font-bold">
         <li class="mx-5 hover:underline"><a href="../index.html">Home</a></li>
         <li class="mx-5 underline text-[#8D2F0D]"><a href="#">Hambúrgueres</a></li>
-        <li class="mx-5 hover:underline"><a href="bebida/index.php">Bebidas</a></li>
-        <li class="mx-5 hover:underline"><a href="sobre-nos.html">Sobre nós</a></li>
+        <li class="mx-5 hover:underline"><a href="../bebida/index.php">Bebidas</a></li>
+        <li class="mx-5 hover:underline"><a href="../sobre-nos.html">Sobre nós</a></li>
       </ol>
     </div>
   </header>
   <main>
     <section class="pb-12">
-      <h2 class="font-[Knewave] text-black text-4xl my-12">CRIAR HAMBÚRGUER</h2>
+      <h2 class="font-[Knewave] text-black text-4xl my-12">EDITAR HAMBÚRGUER</h2>
       <div class="m-0-auto bg-[#17BDB9]">
         <form action="editar-h.php" method="post" class="flex flex-col items-start p-8 m-auto h-auto"> 
             <label class="font-semibold text-2xl" for="nome">Nome</label>
